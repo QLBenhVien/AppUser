9bcbe27f417c2a41a9950e4fb1c236e5fd0d8d0e
phamngocduy <ngocduy14062003@gmail.com>
2024-06-12 10:14:54 +0700

    fixuilogin

--- fe/Login/login.tsx
+++ fe/Login/login.tsx
@@ -1,27 +1,38 @@
-
 import React, { useState } from "react";
-import { Text, View, SafeAreaView, StyleSheet, ImageBackground, TextInput, TouchableOpacity, Alert, ScrollView } from "react-native";
-
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
+const image = { uri: require("../assets/login&register/background.png") };
 
 const LoginScreen = () => {
- 
-
   const handleRegisterPress = () => {
-    Alert.alert('Đăng ký thành công');
+    Alert.alert("Đăng ký thành công");
   };
   const handleLoginPress = () => {
-   Alert.alert('Chuyển trang');
- };
- const handleforgotPassPress = () => {
-   Alert.alert('Chuyển trang');
- };
+    Alert.alert("Chuyển trang");
+  };
+  const handleforgotPassPress = () => {
+    Alert.alert("Chuyển trang");
+  };
 
   return (
     <SafeAreaView style={styles.Container}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <ImageBackground
+        source={require("../assets/login&register/background.png")}
+        style={styles.background}
+        resizeMode="cover"
+      >
         <View style={styles.headercontainer}>
           <ImageBackground
-            source={require('../../fe/assets/logo6 (1).png')}
+            source={require("../../fe/assets/logo6 (1).png")}
             style={styles.logo}
             imageStyle={styles.imglogo}
           />
@@ -29,28 +40,37 @@ const LoginScreen = () => {
         </View>
         <View style={styles.bodycontainer}>
           <View style={styles.Infor}>
-            <TextInput style={styles.Inputinfor} placeholder="Nhập số điện thoại " />
+            <TextInput
+              style={styles.Inputinfor}
+              placeholder="Nhập số điện thoại "
+            />
             <TextInput style={styles.Inputinfor} placeholder="Mật khẩu" />
           </View>
         </View>
         <View style={styles.btnLogin}>
           <TouchableOpacity style={styles.button2} onPress={handleLoginPress}>
-          <Text style={[styles.buttonText, { color: '#ffffff' }]}>Đăng Nhập</Text>
+            <Text style={[styles.buttonText, { color: "#ffffff" }]}>
+              Đăng Nhập
+            </Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.Account}><Text>Bạn đã có tài khoản?</Text></View>
+        <View style={styles.Account}>
+          <Text>Bạn đã có tài khoản?</Text>
+        </View>
         <View style={styles.btnRegister}>
-          <TouchableOpacity style={styles.button1} onPress={handleRegisterPress}>
+          <TouchableOpacity
+            style={styles.button1}
+            onPress={handleRegisterPress}
+          >
             <Text style={styles.buttonText}>Đăng Ký</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.fogetPass}>
-            <TouchableOpacity onPress={handleforgotPassPress}>
-                <Text style={styles.fogetPassText}>Quên mật khẩu?</Text>
-           </TouchableOpacity>
+          <TouchableOpacity onPress={handleforgotPassPress}>
+            <Text style={styles.fogetPassText}>Quên mật khẩu?</Text>
+          </TouchableOpacity>
         </View>
-        
-      </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
@@ -60,16 +80,21 @@ const styles = StyleSheet.create({
   Container: {
     flex: 1,
   },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
   scrollContainer: {
     flexGrow: 1,
   },
   headercontainer: {
-    backgroundColor: '#22668e',
-    height: 250,
-    borderBottomLeftRadius: 100,
-    borderBottomRightRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "35%",
+    justifyContent: "center",
+    alignItems: "center",
   },
   logo: {
     height: 150,
@@ -77,87 +102,86 @@ const styles = StyleSheet.create({
   },
   imglogo: {
     borderRadius: 125,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   textlogo: {
-    color: '#ffffff',
-    paddingTop: 10,
-    fontSize: 30,
-    fontWeight: 'bold',
+    color: "#ffffff",
+    fontSize: 25,
+    fontWeight: "bold",
   },
   bodycontainer: {
     flex: 1,
     marginHorizontal: 15,
-    marginTop: 20,
+    marginTop: 60,
   },
   Infor: {
     paddingHorizontal: 15,
-    paddingTop:20,
+    paddingTop: 20,
   },
   Inputinfor: {
     height: 50,
     paddingVertical: 10,
     paddingHorizontal: 15,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 5,
     fontSize: 16,
     marginVertical: 10,
-    backgroundColor: '#B8CCD7',
+    backgroundColor: "#B8CCD7",
   },
- 
+
   btnRegister: {
     marginHorizontal: 15,
     height: 50,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginVertical: 10,
   },
   button1: {
-    backgroundColor: '#B8CCD7',
+    backgroundColor: "#B8CCD7",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
     marginHorizontal: 15,
   },
   buttonText: {
-    color: '#000000',
+    color: "#000000",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  Account:{
-   justifyContent:'center',
-   alignItems:'center',
-   fontWeight:"bold",
-   color:'Baclk'
+  Account: {
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "bold",
+    color: "Baclk",
   },
-  btnLogin:{
-   marginHorizontal: 15,
+  btnLogin: {
+    marginHorizontal: 15,
     height: 50,
-    justifyContent: 'center',
+    justifyContent: "center",
     marginVertical: 10,
- },
- button2:{
-   backgroundColor: '#22668e',
-   paddingVertical: 10,
-   paddingHorizontal: 20,
-   justifyContent:'center',
-   alignItems:'center',
-   borderRadius: 10,
-   height: '100%',
-   marginHorizontal: 15,
- },
- fogetPass:{
-   paddingHorizontal: 20,
-   justifyContent: 'center',
-   alignItems:'center',
- },
- fogetPassText:{
-   fontSize:18,
-   fontWeight: 'bold',
-   color: '#rgb(0, 0, 255)',
-   textDecorationLine:'underline',
- },
+  },
+  button2: {
+    backgroundColor: "#22668e",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    height: "100%",
+    marginHorizontal: 15,
+  },
+  fogetPass: {
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fogetPassText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#rgb(0, 0, 255)",
+    textDecorationLine: "underline",
+  },
 });
