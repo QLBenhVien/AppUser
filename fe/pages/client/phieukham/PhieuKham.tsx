17de4d2d42c77a5300b1bde9475a6cce5e56ad5d
phamngocduy <ngocduy14062003@gmail.com>
2024-06-26 14:15:33 +0700

    fix luon di

--- fe/pages/client/phieukham/PhieuKham.tsx
+++ fe/pages/client/phieukham/PhieuKham.tsx
@@ -8,37 +8,60 @@ import {
   ScrollView,
   FlatList,
 } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Header from "../../../components/Header";
 import { StatusBar } from "expo-status-bar";
 import ListPhieu from "./components/ListPhieu";
+import Phieu from "../phieukham/DetailPhieuKham";
 const phieuData = [
   {
+    id: 1,
     room: "Phòng 25",
     specialization: "Tai Mũi Họng",
   },
   {
+    id: 2,
     room: "Phòng 20",
     specialization: "Da Liễu",
   },
   {
+    id: 3,
     room: "Phòng 21",
     specialization: "Cấp cứu",
   },
   {
+    id: 4,
     room: "Phòng 23",
     specialization: "Nhi",
   },
   {
+    id: 5,
     room: "Phòng 22",
     specialization: "Thần kinh",
   },
 ];
-const PhieuKham = () => {
+const PhieuKham = ({ navigation }) => {
+  const handlePress = (room: String) => {
+    navigation.navigate("Chi tiết");
+  };
+  const Stack = createNativeStackNavigator();
   return (
     <SafeAreaView style={styles.Container}>
-      <Header content="Phiếu khám bệnh" />
-      <ListPhieu data={phieuData} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#22668E",
+          },
+          headerTintColor: "#fff",
+        }}
+      >
+        <Stack.Screen
+          name="Phiếu khám bệnh"
+          component={() => <ListPhieu data={phieuData} onPress={handlePress} />}
+        />
+        <Stack.Screen name="Chi tiết" component={Phieu} />
+      </Stack.Navigator>
     </SafeAreaView>
   );
 };
