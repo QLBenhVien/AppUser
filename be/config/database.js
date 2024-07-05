0b2a929f4615d40ac2c997d747f949de1ee8c77d
phamngocduy <ngocduy14062003@gmail.com>
2024-07-05 12:00:20 +0700

    add link database

--- be/config/database.js
+++ be/config/database.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 module.exports.connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      "mongodb+srv://ngocduy14062003:FmlkgsIAlH9S1ICh@ngocduy-cnpm.bssbojk.mongodb.net/?retryWrites=true&w=majority&appName=ngocduy-cnpm/app_hospital"
+    );
     console.log("kết nối thành công");
   } catch (error) {
     console.log(error);
