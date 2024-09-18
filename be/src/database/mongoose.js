295b1883e2d79ffe2ac92a712cba161cd414c895
phamngocduy <ngocduy14062003@gmail.com>
2024-09-19 00:05:55 +0700

    move admin

--- be/src/database/mongoose.js
+++ be/src/database/mongoose.js
@@ -6,12 +6,12 @@ mongoose.Promise = global.Promise;
 // Kết nối đến MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error");
     process.exit(1); // Kết thúc ứng dụng với mã lỗi nếu không kết nối được
   }
-}; 
+};
 
 module.exports = connectDB;
